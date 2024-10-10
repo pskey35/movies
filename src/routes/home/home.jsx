77809f505619bfa06da8e292fbff02dfc272ef9c
pskey35 <anonimo35371@gmail.com>
2024-10-10 16:03:51 -0500

    chore

---
+++ src/routes/home/home.jsx
@@ -0,0 +1,10 @@
+import "./home.scss"
+export default function Home(){
+    return (
+        <div className="ventana_home">
+            <div className="ventanaContent_home">
+
+            </div>
+        </div>
+    )
+}

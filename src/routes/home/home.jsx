5da149bc022aade1825895d7deec461cebb2d01c
pskey35 <anonimo35371@gmail.com>
2024-10-14 20:55:17 -0500

    feat(home.jsx): se agrego un trailer para el home

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,8 +1,96 @@
+import { useState, useEffect } from "react"
 import "./home.scss"
-export default function Home(){
+export default function Home() {
+
+    //este es el indice para cambiar de videos en el trailer
+    const [listaPlayIndice, setListaPlayIndice] = useState(0)
+
+    //se necesita hacer el trailer que sean 5 videos
+    //que intercambien entre si con opacity
+
+    const listaAudio = [
+        "https://media.istockphoto.com/id/1752211407/es/v%C3%ADdeo/una-atleta-fuerte-corre-hacia-un-obst%C3%A1culo-saltando-la-barrera-a-gran-velocidad-mientras.mp4?s=mp4-640x640-is&k=20&c=FKSbQNRstNkg38m4858zYNFdIed-IHub02NlrMj_TiU=",
+        "https://cdn.pixabay.com/video/2017/08/09/11256-229053646_large.mp4",
+        "https://media.istockphoto.com/id/1364131749/es/v%C3%ADdeo/maestra-de-secundaria-supervisando-a-los-estudiantes-que-toman-el-examen-en-los-escritorios.mp4?s=mp4-640x640-is&k=20&c=JrS3VueJVkDU-a_ye9WnudEp2hXNLecpARBb7TMYZ1k=",
+        "https://cdn.pixabay.com/video/2015/11/27/1406-147169807_medium.mp4",
+    ]
+
+
+    useEffect(() => {
+
+
+
+
+
+
+        const interval = setInterval(() => {
+            //cada 3 segundos se cambiara de video
+            setListaPlayIndice(prevState => {
+
+                if (prevState == listaAudio.length - 1) {
+                    //si la listaAudio llega al final del video se reinicia desde el primero
+                    console.log("entro a if prevState igual a listaAudio.length - 1")
+                    console.log(prevState, "---", listaAudio.length - 1)
+                    console.log(listaAudio[prevState])
+                    return 0
+                }
+
+
+
+                return prevState + 1
+            })
+        }, 6000)
+
+
+
+        const video_trailer_home = document.querySelector("#video-trailer_home")
+        
+        video_trailer_home.style.opacity = "0"
+
+        const time = setTimeout(() => {
+            video_trailer_home.style.opacity = "1"
+        }, 900)
+
+
+
+        if (video_trailer_home) {
+
+            video_trailer_home.load()
+            video_trailer_home.play()
+            // console.log(listaAudio[listaPlayIndice])
+        }
+
+
+        return () => {
+            clearInterval(interval)
+            clearTimeout(time)
+        }
+
+
+
+
+
+    }, [, listaPlayIndice])
+
+
+    /*
+        const finalVideo = ()=>{
+            const video_trailer_home = document.querySelector("#video-trailer_home")
+            video_trailer_home.style.opacity = "0"
+    
+            setTimeout(()=>{
+                video_trailer_home.style.opacity = "1"
+    
+            },1000)
+        }*/
     return (
         <div className="ventana_home">
             <div className="ventanaContent_home">
+                <div className="videoContainer_home">
+                    <video id="video-trailer_home" >
+                        <source src={listaAudio[listaPlayIndice]}></source>
+                    </video>
+                </div>
 
             </div>
         </div>

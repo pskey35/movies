a819b309af8b7629d7eeca364cd9f452e3f482b8
pskey35 <anonimo35371@gmail.com>
2024-10-18 16:36:21 -0500

    styles:se mejoro solamente la UI de la ruta home

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -28,7 +28,7 @@ export default function Home() {
 
     useEffect(() => {
         const video_trailer_home = document.querySelector("#video-trailer_home")
-        
+
         const interval = setInterval(() => {
             video_trailer_home.style.opacity = 1
             //cada 3 segundos se cambiara de video
@@ -43,13 +43,13 @@ export default function Home() {
                 }
                 return prevState + 1
             })
-            
+
         }, 6000)
-        const time = setTimeout(()=>{
+        const time = setTimeout(() => {
             video_trailer_home.style.opacity = 0
         }, 5500)
         //video_trailer_home.classList.add('fadeOut')   
-        
+
 
         if (video_trailer_home) {
 
@@ -91,9 +91,82 @@ export default function Home() {
     const listaDataVideo = [
         {
             id: "id-1",
-            name: "The avengers",
-            info: "lorem ipsum dolor aismet",
+            name: "The Avengers",
+            info: "Earth's mightiest heroes must come together to fight against a global threat.",
             imageUrl: "https://lumiere-a.akamaihd.net/v1/images/eu_disneyplus_avengers-endgame_mob_m_928f44f1.jpeg?region=100,0,600,600"
+        },
+        {
+            id: "id-2",
+            name: "Inception",
+            info: "A thief who steals corporate secrets through dream-sharing technology is given the inverse task of planting an idea into a CEO's mind.",
+            imageUrl: "https://m.media-amazon.com/images/M/MV5BMjExMjkwNTQ0Nl5BMl5BanBnXkFtZTcwNTY0OTk1Mw@@._V1_.jpg"
+        },
+        {
+            id: "id-3",
+            name: "The Dark Knight",
+            info: "Batman must accept one of the greatest psychological and physical tests when he faces the Joker.",
+            imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsqSpHZPdmKW1v-rCuAT0PmnwFxy9dKCVscQ&s"
+        },
+        {
+            id: "id-8",
+            name: "Star Wars: A New Hope",
+            info: "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee, and two droids to save the galaxy.",
+            imageUrl: "https://m.media-amazon.com/images/I/71rNJQ2g-EL._AC_SY679_.jpg"
+        },
+        {
+            id: "id-9",
+            name: "BPRUEBA",
+            info: "Marty McFly, a 17-year-old high school student, is accidentally sent 30 years into the past in a time-traveling DeLorean.",
+            imageUrl: "https://m.media-amazon.com/images/S/pv-target-images/bcbe99ca1bdf7a28b033fab7ac55983de9c46f3c0ec95871d8f4d18b7e752d5d.jpg"
+        },
+        {
+            id: "id-6",
+            name: "The Matrix",
+            info: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+            imageUrl: "https://m.media-amazon.com/images/I/51EG732BV3L.jpg"
+        },
+        {
+            id: "id-7",
+            name: "Titanic",
+            info: "A young aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.",
+            imageUrl: "https://m.media-amazon.com/images/I/61i8Vjb17SL._AC_SY679_.jpg"
+        },
+        {
+            id: "id-8",
+            name: "Star Wars: A New Hope",
+            info: "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee, and two droids to save the galaxy.",
+            imageUrl: "https://m.media-amazon.com/images/I/71rNJQ2g-EL._AC_SY679_.jpg"
+        },
+        {
+            id: "id-9",
+            name: "Back to the Future",
+            info: "Marty McFly, a 17-year-old high school student, is accidentally sent 30 years into the past in a time-traveling DeLorean.",
+            imageUrl: "https://m.media-amazon.com/images/S/pv-target-images/bcbe99ca1bdf7a28b033fab7ac55983de9c46f3c0ec95871d8f4d18b7e752d5d.jpg"
+        },
+
+    ]
+
+
+    const generosPopulares = [
+        {
+            id: "waza-1",
+            nombreGenero: "Anime",
+            img: "https://m.media-amazon.com/images/S/pv-target-images/bcbe99ca1bdf7a28b033fab7ac55983de9c46f3c0ec95871d8f4d18b7e752d5d.jpg"
+        },
+        {
+            id: "waza-1",
+            nombreGenero: "Anime",
+            img: "https://m.media-amazon.com/images/S/pv-target-images/bcbe99ca1bdf7a28b033fab7ac55983de9c46f3c0ec95871d8f4d18b7e752d5d.jpg"
+        },
+        {
+            id: "waza-1",
+            nombreGenero: "Anime",
+            img: "https://m.media-amazon.com/images/S/pv-target-images/bcbe99ca1bdf7a28b033fab7ac55983de9c46f3c0ec95871d8f4d18b7e752d5d.jpg"
+        },
+        {
+            id: "waza-1",
+            nombreGenero: "Anime",
+            img: "https://m.media-amazon.com/images/S/pv-target-images/bcbe99ca1bdf7a28b033fab7ac55983de9c46f3c0ec95871d8f4d18b7e752d5d.jpg"
         }
     ]
 
@@ -137,23 +210,50 @@ export default function Home() {
                     </div>
                 </div>
                 <div className="section-2_home">
-                    <div className="barra_home">
-                        <div>
-                            Generos populares
-                        </div>
-                        <span>
+                    <div>
 
-                        </span>
-                        <div>
-                            Generos
+
+                        <div className="barra_home">
+                            <div>
+                                Generos populares
+                            </div>
+                            <span>
+
+                            </span>
+                            <div>
+                                Generos
+                            </div>
+                        </div>
+                        <div className="generosPopulares_home">
+                            {generosPopulares.map((dataUnidad, index) => {
+                                return (
+                                    <div className="genero_home">
+                                        <h7>{dataUnidad.nombreGenero}</h7>
+                                    </div>
+                                )
+                            })}
                         </div>
                     </div>
+
+
+
+
                     <div className="galeriaVideos_home">
+                        <h1>Recien agregadas</h1>
                         <div className="galeriaVideos-content_home">
                             {listaDataVideo.map((dataUnidad) =>
                                 <div className="itemVideo_home" key={dataUnidad.id}>
                                     <img src={dataUnidad.imageUrl}>
                                     </img>
+                                    <div className="showHover_home">
+                                        <div>
+                                            Pelicula ...
+                                        </div>
+                                        <div>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing
+                                            elit. Provident numquam quas cum accusamus ipsa vel?
+                                        </div>
+                                    </div>
                                 </div>
                             )}
 

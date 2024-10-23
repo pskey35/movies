976b11100abb20400e8db5459b8522a5b83033b4
pskey35 <anonimo35371@gmail.com>
2024-10-23 10:13:06 -0500

    feat(header.jsx): se agrego una funcion del evento onKeyUp para que cuando presione enter este busque en la ruta /saerch

--- components/header.jsx
+++ components/header.jsx
@@ -3,6 +3,7 @@ import { useNavigate, Link } from 'react-router-dom';
 import "./header.scss"
 import { ContextGlobal } from "../src/App";
 
+
 export default function Header() {
 
 
@@ -99,6 +100,13 @@ export default function Header() {
 
 
 
+    const keyUpLupa = (event)=>{
+        if(event.keyCode == 13){
+            navigate(`/search?query=${event.target.value}`)
+           // navigate()
+        }
+    }
+
     return (
         <div className="nav-header">
             <div className="nav-header-content">
@@ -123,7 +131,7 @@ export default function Header() {
                 </div>
                 <div className="nav-right">
                     <div className="lupaIcon-header">
-                        <input className="input-element"></input>
+                        <input className="input-element" onKeyUp={keyUpLupa}></input>
                         <div className="lupa-icon" onClick={clickLupa}>
                             <svg class="icono-busqueda " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                 <path fill="#ffffff" d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"></path>

59304c7db1da0e527abd660e2e5cd01e25c0a243
pskey35 <anonimo35371@gmail.com>
2024-10-23 08:34:14 -0500

    feat(App.jsx): se ha comenzado a usar un estado global con useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, createContext} from 'react'
 import Header from "../components/header"
 import './App.scss'
 import { Routes, Route } from "react-router-dom"
@@ -6,23 +6,36 @@ import Home from "./routes/home/home.jsx"
 import Login from "./routes/login/login.jsx"
 
 
+
+export const ContextGlobal = createContext()
+
 function App() {
+
+
+
+  const [isTokenValid,setIsTokenValid] = useState(null)
+
 
 
   return (
 
-    <div className="ventana">
+    <ContextGlobal.Provider value={{
+      isTokenValid,
+      setIsTokenValid
+    }}>
+      <div className="ventana">
 
 
 
-      <Routes>
+        <Routes>
 
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/login" element={<Login></Login>}></Route>
-      </Routes>
-      
-     
-    </div>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/login" element={<Login></Login>}></Route>
+        </Routes>
+
+
+      </div>
+    </ContextGlobal.Provider>
   )
 }
 
